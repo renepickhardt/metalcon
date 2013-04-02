194fb415fdb030ecf40110bbbc6be18aaa511a06
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2013-04-02 05:22:57 -0700

    initial project configuration: Java & Maven

---
+++ src/main/java/de/uniko/west/socialsensor/graphity/server/Launcher.java
@@ -0,0 +1,16 @@
+package de.uniko.west.socialsensor.graphity.server;
+
+/**
+ * server launcher<br>
+ * TODO: check if necessary when switching to Tomcat
+ * 
+ * @author sebschlicht
+ * 
+ */
+public class Launcher {
+
+	public static void main(String[] args) {
+		System.out.println("Tomcat not supported yet!");
+	}
+
+}
