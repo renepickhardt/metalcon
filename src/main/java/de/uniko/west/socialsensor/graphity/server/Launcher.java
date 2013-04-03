657ad649ac6b2855517ca83df2862c3733a7fa65
sebschlicht <sebschlicht@uni-koblenz.de>
2013-04-03 04:51:30 -0700

    neo4j has been added to the project

--- src/main/java/de/uniko/west/socialsensor/graphity/server/Launcher.java
+++ src/main/java/de/uniko/west/socialsensor/graphity/server/Launcher.java
@@ -1,5 +1,8 @@
 package de.uniko.west.socialsensor.graphity.server;
 
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
 /**
  * server launcher<br>
  * TODO: check if necessary when switching to Tomcat
@@ -9,7 +12,25 @@ package de.uniko.west.socialsensor.graphity.server;
  */
 public class Launcher {
 
+	/**
+	 * database path
+	 */
+	private static final String DB_PATH = "target/database/location";
+
+	private static void registerShutdownHook(final GraphDatabaseService graphDB) {
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				graphDB.shutdown();
+			}
+		});
+	}
+
 	public static void main(String[] args) {
+		GraphDatabaseService graphDB = new GraphDatabaseFactory()
+				.newEmbeddedDatabase(DB_PATH);
+		registerShutdownHook(graphDB);
+
 		System.out.println("Tomcat not supported yet!");
 	}
 
