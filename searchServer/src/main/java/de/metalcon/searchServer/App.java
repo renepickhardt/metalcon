9ff9852872ef99dedcbb5d58198d3567f87e63aa
Rene Pickhardt <rene@rene-pickhardt.de>
2013-08-01 11:56:23 +0200

    initial maven project with nutch as dependency

---
+++ searchServer/src/main/java/de/metalcon/searchServer/App.java
@@ -0,0 +1,13 @@
+package de.metalcon.searchServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
