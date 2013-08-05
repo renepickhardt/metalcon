7cedd3614519d854eae0caea392a8ea031e91402
Rene Pickhardt <rene@rene-pickhardt.de>
2013-08-06 00:02:27 +0200

    fixes deploy.sh such that war container ist first copied if tomcat is down

--- autocompleteServer/deploy.sh
+++ autocompleteServer/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mvn compile
 mvn package war:war
+sudo /etc/init.d/tomcat7 stop
 sudo cp target/autocompleteServer-0.0.1-SNAPSHOT.war /var/lib/tomcat7/webapps/autocompleteServer-0.0.1-SNAPSHOT.war
-sudo /etc/init.d/tomcat7 restart
+sudo /etc/init.d/tomcat7 start
