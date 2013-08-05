a83b735ba0eaaa006af823f82377a0529ac09f10
Rene Pickhardt <rene@rene-pickhardt.de>
2013-08-05 23:51:40 +0200

    cleaned up the code

--- autocompleteServer/src/main/java/de/metalcon/autocompleteServer/Search.java
+++ autocompleteServer/src/main/java/de/metalcon/autocompleteServer/Search.java
@@ -9,44 +9,45 @@ public class Search {
 
 	public static String treeSearch(String input) {
 		if (suggestTree == null) {
-			suggestTree = new SuggestTree(7);
-			int priority = 100000;
-			String filename = "/var/lib/datasets/metalcon/Band.csv";
-			// there must be a way to use relative paths!
-
-			try {
-				BufferedReader in = new BufferedReader(new FileReader(filename));
-				String zeile = null;
-				String newZeile = null;
-				String newZeileMyspace = null;
-				// String Bandname = null;
-
-				while ((zeile = in.readLine()) != null) {
-					String[] parts = zeile.split(";");
-					if ((parts.length == 3) && !(parts[1].equals("NULL"))
-							&& !(parts[2].equals("NULL"))
-							&& !(parts[0].equals(""))) {
-						parts[0] = parts[0].replaceAll("[\"]", "");
-						newZeile = "<a href=" + parts[1] + ">" + parts[0]
-								+ "</a>" + "<br>";
-						newZeileMyspace = "<a href=" + parts[2] + ">"
-								+ parts[0] + "</a>" + "<br>";
-						suggestTree.put(parts[0], priority);
-						--priority;
-					}
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-				return null;
-			}
+			initilizeSuggestTree();
 		}
-		// TODO: find a working way to return multiple results
 		Node result = suggestTree.getBestSuggestions(input);
 		StringBuffer resultList = new StringBuffer("");
 		for (int i = 0; i < result.listLength(); ++i) {
 			resultList = resultList.append(result.getSuggestion(i) + ",");
 		}
-		// return resultList.toString();
 		return resultList.toString();
 	}
+
+	private static void initilizeSuggestTree() {
+		suggestTree = new SuggestTree(7);
+		int priority = 100000;
+		String filename = "/var/lib/datasets/metalcon/Band.csv";
+		// there must be a way to use relative paths!
+
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(filename));
+			String zeile = null;
+			String newZeile = null;
+			String newZeileMyspace = null;
+			// String Bandname = null;
+
+			while ((zeile = in.readLine()) != null) {
+				String[] parts = zeile.split(";");
+				if ((parts.length == 3) && !(parts[1].equals("NULL"))
+						&& !(parts[2].equals("NULL"))
+						&& !(parts[0].equals(""))) {
+					parts[0] = parts[0].replaceAll("[\"]", "");
+					newZeile = "<a href=" + parts[1] + ">" + parts[0]
+							+ "</a>" + "<br>";
+					newZeileMyspace = "<a href=" + parts[2] + ">"
+							+ parts[0] + "</a>" + "<br>";
+					suggestTree.put(parts[0], priority);
+					--priority;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}		
 }
