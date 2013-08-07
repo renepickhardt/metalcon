1a19e8c6d5ed6cceadcb52429addf1ddcf0b2047
Rene Pickhardt <rene@rene-pickhardt.de>
2013-08-07 22:40:57 +0200

    implemented more of the specifications of the retrieval part of the ASTP Server

--- autocompleteServer/src/main/java/de/metalcon/autocompleteServer/Helper/StatusCodes.java
+++ autocompleteServer/src/main/java/de/metalcon/autocompleteServer/Helper/StatusCodes.java
@@ -12,5 +12,10 @@ public class StatusCodes {
 	public static final String NUMITEMS_NOT_AN_INTEGER = "The numItems Parameter needs to be an Integer. I set it to: " + ProtocolConstants.MAX_NUMBER_OF_SUGGESTIONS;
 	public static final String NUMITEMS_NOT_GIVEN = "You did not give the number of items I set it to: "+ ProtocolConstants.MAX_NUMBER_OF_SUGGESTIONS;
 	public static final String NUMITEMS_OUT_OF_RANGE = "The rquested number of items is out of range. It has to be between 1 and " + ProtocolConstants.MAX_NUMBER_OF_SUGGESTIONS + " I set it to: "+ ProtocolConstants.MAX_NUMBER_OF_SUGGESTIONS;
-
+	
+	public static final String NO_INDEX_GIVEN = "You did not provide the index Field. I set it to the default index named: " + ProtocolConstants.DEFAULT_INDEX_NAME;
+	public static final String INDEX_UNKNOWN = "The index that you want to query from is unknown to me. I used the default index named: " + ProtocolConstants.DEFAULT_INDEX_NAME;
+	public static final String NO_INDEX_AVAILABLE = "The given index is not known to the server ADN / OR the default index is not up. Check if the server is correctly configured.";
+	
+	public static final String NO_TERM_GIVEN = "I cannot give you suggestions if you don't provide me a prefix";
 }
