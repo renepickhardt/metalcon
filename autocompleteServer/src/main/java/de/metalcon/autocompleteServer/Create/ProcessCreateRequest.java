9f4d2eb37417af20d296caf0dcea55b14a3cb417
CSchowalter <cschowalter@uni-koblenz.de>
2013-08-08 17:58:49 +0200

    Implemented basic structure for Create, including (Blocking)Queue and(Create)Servlet. Also adapted the web.xml accordingly.

--- autocompleteServer/src/main/java/de/metalcon/autocompleteServer/Create/ProcessCreateRequest.java
+++ autocompleteServer/src/main/java/de/metalcon/autocompleteServer/Create/ProcessCreateRequest.java
@@ -1,57 +1,70 @@
 package de.metalcon.autocompleteServer.Create;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
 
-import de.metalcon.autocompleteServer.Helper.ProtocolConstants;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  * 
  * @author Christian Schowalter
- *
+ * 
  */
 public class ProcessCreateRequest {
 
+	public void checkRequestParameter(HttpServletRequest request) {
+		// checkContentType(); Accept must be text/x-json.
 
-	public static void checkRequestParameter(HttpServletRequest request){
-		//checkContentType();	Accept muss text/x-json sein.
-		
-
+		// Uncomment this check for more paranoid behavior (yet to be
+		// specified!)
+		// boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+		// if (isMultipart) {}
 
 	}
-	/**
-	 * checks if the request parameters follow the protocol or corrects them.
-	 * @param request
-	 * @throws IOException 
-	 */
-	public static void checkRequestBody(HttpServletRequest request) throws IOException{
-		String line = null;
-		BufferedReader reader = request.getReader();
-		while ((line = reader.readLine()) != null) {
-			String[] parts = line.split("--ASTP-Boundary");
-			
+
+	private final DiskFileItemFactory factory = new DiskFileItemFactory();
+
+	private FormItemList extractFormItems(final HttpServletRequest request) {
+		final ServletFileUpload upload = new ServletFileUpload(this.factory);
+		final FormItemList formItems = new FormItemList();
+
+		try {
+			for (FileItem item : upload.parseRequest(request)) {
+				if (item.isFormField()) {
+					formItems.addField(item.getFieldName(), item.getString());
+				} else {
+					formItems.addFile(item.getFieldName(), item);
+				}
+			}
+		} catch (FileUploadException e) {
+			// TODO make status message instead of STrace
+			e.printStackTrace();
+		} catch (FormItemDoubleUsageException e) {
+			// TODO make status message instead of STrace
+			e.printStackTrace();
 		}
 
+		return formItems;
 	}
+
 	private static void checkImage(HttpServletRequest request) {
 		// This method needs a library to analyze JPEGs concerning their size.
-		
+
 	}
+
 	private static void checkIndex(HttpServletRequest request) {
-
+		// Check if index is provided. If not, use default and respond
 	}
+
 	private static void checkKey(HttpServletRequest request) {
-
+		// Check if key is provided. Wait, is this even necessary?
 	}
+
 	private static void checkTerm(HttpServletRequest request) {
-		// TODO Auto-generated method stub
+		// Check if term is provided. If not, abort and respond
 
 	}
-	/**
-	 * checks the HTTP-body of a create request for its correctness.
-	 * @param request
-	 */
 
 }
---
+++ autocompleteServer/src/main/java/de/metalcon/autocompleteServer/Server.java
@@ -0,0 +1,29 @@
+package de.metalcon.autocompleteServer;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class Server implements ServletContextListener {
+
+	private BlockingQueue<Command> commands;
+	private Worker worker;
+
+	public Server() {
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		this.commands = new LinkedBlockingQueue<Command>();
+		this.worker = new Worker(this.commands);
+	}
+
+}
