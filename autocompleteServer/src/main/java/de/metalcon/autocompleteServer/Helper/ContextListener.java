f20948f3ca52b108664ee4a791ea4395ef86bc31
Rene Pickhardt <rene@rene-pickhardt.de>
2013-08-11 11:02:25 +0200

    added some dummy code for tests. This is not a test yet but rather some experiments on how to set up a servlet testing environment

--- autocompleteServer/src/main/java/de/metalcon/autocompleteServer/Helper/ContextListener.java
+++ autocompleteServer/src/main/java/de/metalcon/autocompleteServer/Helper/ContextListener.java
@@ -13,9 +13,16 @@ public class ContextListener implements ServletContextListener {
 	public static SuggestTree getIndex(String indexName, ServletContext context){
 		return (SuggestTree)context.getAttribute("indexName:"+indexName);
 	}
+	public static void setIndex(String indexName, SuggestTree index, ServletContext context){
+		context.setAttribute("indexName:" + indexName, index);
+	}
 	
 	public static HashMap<String, String> getImageIndex(ServletContext context){
 		return (HashMap<String, String>)context.getAttribute(ProtocolConstants.IMAGE_SERVER_CONTEXT_KEY);
+	}
+	
+	public static void setImageIndex(HashMap<String, String> imageIndex, ServletContext context){
+		context.setAttribute(ProtocolConstants.IMAGE_SERVER_CONTEXT_KEY, imageIndex);
 	}
 	
 	public void contextDestroyed(ServletContextEvent arg0){
---
+++ autocompleteServer/src/test/java/de/metalcon/autocompleteServer/Retrieve/TestProcessRetrieveRequest.java
@@ -0,0 +1,135 @@
+/**
+ * 
+ */
+package de.metalcon.autocompleteServer.Retrieve;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.junit.Test;
+import org.mockito.stubbing.OngoingStubbing;
+
+import de.metalcon.autocompleteServer.Helper.ContextListener;
+import de.metalcon.autocompleteServer.Helper.ProtocolConstants;
+import de.metalcon.autocompleteServer.Helper.SuggestTree;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Rene Pickhardt
+ * 
+ */
+public class TestProcessRetrieveRequest {
+
+	/**
+	 * Test method for
+	 * {@link de.metalcon.autocompleteServer.Retrieve.ProcessRetrieveRequest#checkRequestParameter(javax.servlet.http.HttpServletRequest, javax.servlet.ServletContext)}
+	 * . foolowing:
+	 * http://stackoverflow.com/questions/5434419/how-to-test-my-servlet
+	 * -using-junit
+	 * @throws ServletException 
+	 */
+	@Test
+	public void testCheckRequestParameter() throws ServletException {
+
+		final ServletConfig servletConfig = mock(ServletConfig.class);
+		final ServletContext servletContext = mock(ServletContext.class);
+
+		// setup the test.
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		HttpServlet servlet = mock(HttpServlet.class);
+		when(servletConfig.getServletContext()).thenReturn(servletContext);
+
+		SuggestTree generalIndex = new SuggestTree(7);
+		generalIndex.put("Metallica", 100, "Metallica");
+		generalIndex.put("Megadeth", 99, "Metallica");
+		generalIndex.put("Megaherz", 98, "Metallica");
+		generalIndex.put("Meshuggah", 97, "Metallica");
+		generalIndex.put("Menhir", 96, "Metallica");
+		generalIndex.put("Meat Loaf", 95, "Metallica");
+		generalIndex.put("Melechesh", 94, "Metallica");
+
+		when(servletContext.getAttribute("indexName:"+ProtocolConstants.DEFAULT_INDEX_NAME)).thenReturn(
+				generalIndex);
+		
+		SuggestTree venueIndex = new SuggestTree(7);
+		venueIndex.put("Das Kult", 55, "http://www.daskult.de");
+		venueIndex.put("Die Halle", 44, "http://www.diehalle-frankfurt.de");
+		venueIndex.put("Die Mühle", 30, "http://www.die-muehle.net");
+		venueIndex.put("Dreams", 30, "http://www.discothek-dreams.de");
+		venueIndex.put("Dudelsack", 27, "http://www.dudelsack-bk.de");
+		venueIndex.put("Das Haus", 25, "http://www.dashaus-lu.de/home.html");
+		venueIndex.put("Druckluftkammer", 25, "http://www.druckluftkammer.de/");
+
+		when(servletContext.getAttribute("indexName:"+"venueIndex")).thenReturn(
+				venueIndex);
+
+		HashMap<String, String> imageIndex = new HashMap<String, String>();
+		when(servletContext.getAttribute(ProtocolConstants.IMAGE_SERVER_CONTEXT_KEY)).thenReturn(imageIndex);
+		
+		servlet.init(servletConfig);		
+
+		
+		//		ContextListener.setIndex("venues", venueIndex, context);
+
+		/***********************************************************************************************
+		 * DO THE TESTING FROM HERE
+		 ***********************************************************************************************/
+		when(request.getParameter("term")).thenReturn("Me");
+		when(request.getParameter("numItems")).thenReturn("7");
+		when(request.getParameter("indexName")).thenReturn("generalindex");
+		SuggestTree index = ContextListener.getIndex(
+				ProtocolConstants.DEFAULT_INDEX_NAME, servletContext);
+		assertNotNull(index);
+		ProcessRetrieveResponse response = ProcessRetrieveRequest
+				.checkRequestParameter(request, servletContext);
+		response.buildJsonResonse();
+		JSONObject jsonResponse = getJson(response);
+		ArrayList<HashMap<String, String>> suggestionList = (ArrayList<HashMap<String, String>>) jsonResponse.get("suggestionList");
+		assertTrue(suggestionList.size()==7);
+
+	}
+
+	/**
+	 * @param response
+	 * @return
+	 */
+	private JSONObject getJson(ProcessRetrieveResponse response) {
+		Field field = null;
+		try {
+			field = ProcessRetrieveResponse.class
+					.getDeclaredField("jsonResponse");
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+			fail("could not test the Json resonse of the ProcessRetrieveResponse class");
+		} catch (SecurityException e) {
+			e.printStackTrace();
+			fail("could not test the Json resonse of the ProcessRetrieveResponse class");
+		}
+		field.setAccessible(true);
+		try {
+			return (JSONObject) field.get(response);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			fail("could not test the Json resonse of the ProcessRetrieveResponse class");
+
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+			fail("could not test the Json resonse of the ProcessRetrieveResponse class");
+		}
+		return null;
+	}
+
+}
