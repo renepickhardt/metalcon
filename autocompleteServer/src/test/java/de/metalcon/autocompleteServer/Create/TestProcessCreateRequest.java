b38271c09baf8604d90d93957766ac253ed3db2d
CSchowalter <cschowalter@uni-koblenz.de>
2013-08-20 00:49:23 +0200

    corrected formatting (via save action)

--- autocompleteServer/src/test/java/de/metalcon/autocompleteServer/Create/TestProcessCreateRequest.java
+++ autocompleteServer/src/test/java/de/metalcon/autocompleteServer/Create/TestProcessCreateRequest.java
@@ -21,8 +21,6 @@ public class TestProcessCreateRequest {
 	final private ServletConfig servletConfig = mock(ServletConfig.class);
 	final private ServletContext servletContext = mock(ServletContext.class);
 
-
-	
 	private HttpServletRequest initializeTest() {
 
 		HttpServletRequest request = mock(HttpServletRequest.class);
@@ -32,7 +30,9 @@ public class TestProcessCreateRequest {
 				this.servletContext);
 		when(this.servletConfig.getServletContext()).thenReturn(
 				this.servletContext);
-		when(servletContext.getAttribute("indexName:"+ProtocolConstants.DEFAULT_INDEX_NAME)).thenReturn(
+		when(
+				this.servletContext.getAttribute("indexName:"
+						+ ProtocolConstants.DEFAULT_INDEX_NAME)).thenReturn(
 				generalIndex);
 
 		try {
@@ -46,78 +46,84 @@ public class TestProcessCreateRequest {
 
 	@Test
 	public void testContainerRun() {
-		
-		//TODO: include an image to this test or make another one with an image
+
+		// TODO: include an image to this test or make another one with an image
 		HttpServletRequest request = this.initializeTest();
-		ProcessCreateResponse response = new ProcessCreateResponse(this.servletConfig.getServletContext());
-//		ProcessCreateResponse response = ProcessCreateRequest
-//				.checkRequestParameter(request,
-//						this.servletConfig.getServletContext());
-		
+		ProcessCreateResponse response = new ProcessCreateResponse(
+				this.servletConfig.getServletContext());
+		// ProcessCreateResponse response = ProcessCreateRequest
+		// .checkRequestParameter(request,
+		// this.servletConfig.getServletContext());
+
 		CreateRequestContainer suggestTreeCreateRequestContainer = new CreateRequestContainer();
-		
+
 		suggestTreeCreateRequestContainer.setSuggestString("testband");
 		suggestTreeCreateRequestContainer.setIndexName("testIndex");
 		suggestTreeCreateRequestContainer.setKey("testkey");
 		suggestTreeCreateRequestContainer.setWeight(1);
-		
-//		if (suggestTreeCreateRequestContainer == null){
-//			System.out.println("Container null");
-//			if (suggestTreeCreateRequestContainer.getIndexName() == null){
-//				System.out.println("index null");
-//
-//			}
-//		}
-		
+
+		// if (suggestTreeCreateRequestContainer == null){
+		// System.out.println("Container null");
+		// if (suggestTreeCreateRequestContainer.getIndexName() == null){
+		// System.out.println("index null");
+		//
+		// }
+		// }
+
 		// FIXME: This NullPointerException must vanish!
-		suggestTreeCreateRequestContainer.run(this.servletConfig.getServletContext());
-		
-		String retrieveResponse =  servletConfig.getServletContext().getAttribute("indexName:"+ProtocolConstants.DEFAULT_INDEX_NAME).toString(); //generalIndex.getBestSuggestions("test").toString();
-		
+		suggestTreeCreateRequestContainer.run(this.servletConfig
+				.getServletContext());
+
+		String retrieveResponse = this.servletConfig
+				.getServletContext()
+				.getAttribute(
+						"indexName:" + ProtocolConstants.DEFAULT_INDEX_NAME)
+				.toString(); // generalIndex.getBestSuggestions("test").toString();
+
 		System.out.println(retrieveResponse);
-		
-		assertTrue(retrieveResponse.equals("{\"Status\":\"OK\",\"term\":\"testband\"}"));
-		
+
+		assertTrue(retrieveResponse
+				.equals("{\"Status\":\"OK\",\"term\":\"testband\"}"));
+
 	}
-	
-	
-	
-//	@Test
-//	public void testFullFormWithoutImage() {
-//		HttpServletRequest request = this.initializeTest();
-//
-//		when(request.getParameter("term")).thenReturn("testband");
-//		when(request.getParameter("key")).thenReturn("testkey");
-//		when(request.getParameter("weight")).thenReturn("1");
-//		when(request.getParameter("index")).thenReturn("testIndex");
-//
-//		ProcessCreateResponse response = new ProcessCreateResponse(this.servletConfig.getServletContext());
-//
-//
-//		 if (response != null) {
-//		 if (response.getContainer() != null) {
-//		 CreateRequestContainer resp = response.getContainer();
-//		 if (resp.getKey() != null) {
-//		 assertTrue(response.getContainer().getKey()
-//		 .equals("testkey"));
-//		 } else {
-//		 System.out.println(response.getResponse().toString());
-//		 System.out.println("key null");
-//		 }
-//		 } else {
-//		 System.out.println("resp null");
-//		 }
-//		 } else {
-//		 System.out.println("response null");
-//		 }
-//
-//		// FIXME: The Form-check recognizes this not being actual form-data and
-//		// thus throws its exception
-//		assertTrue(response.getContainer().getKey().equals("testKey"));
-//		assertTrue(response.getContainer().getSuggestString()
-//				.equals("testband"));
-//		assertTrue(response.getContainer().getWeight().equals("1"));
-//		assertTrue(response.getContainer().getIndexName().equals("testIndex"));
-//
-//	}
+
+	// @Test
+	// public void testFullFormWithoutImage() {
+	// HttpServletRequest request = this.initializeTest();
+	//
+	// when(request.getParameter("term")).thenReturn("testband");
+	// when(request.getParameter("key")).thenReturn("testkey");
+	// when(request.getParameter("weight")).thenReturn("1");
+	// when(request.getParameter("index")).thenReturn("testIndex");
+	//
+	// ProcessCreateResponse response = new
+	// ProcessCreateResponse(this.servletConfig.getServletContext());
+	//
+	//
+	// if (response != null) {
+	// if (response.getContainer() != null) {
+	// CreateRequestContainer resp = response.getContainer();
+	// if (resp.getKey() != null) {
+	// assertTrue(response.getContainer().getKey()
+	// .equals("testkey"));
+	// } else {
+	// System.out.println(response.getResponse().toString());
+	// System.out.println("key null");
+	// }
+	// } else {
+	// System.out.println("resp null");
+	// }
+	// } else {
+	// System.out.println("response null");
+	// }
+	//
+	// // FIXME: The Form-check recognizes this not being actual form-data and
+	// // thus throws its exception
+	// assertTrue(response.getContainer().getKey().equals("testKey"));
+	// assertTrue(response.getContainer().getSuggestString()
+	// .equals("testband"));
+	// assertTrue(response.getContainer().getWeight().equals("1"));
+	// assertTrue(response.getContainer().getIndexName().equals("testIndex"));
+	//
+	// }
 }
