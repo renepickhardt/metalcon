2e94afddf71d899c1495e3102121f8800c874eeb
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2013-08-19 17:34:44 +0200

    This exception throw wasn't needed.

--- searchServer/src/main/java/de/metalcon/searchServer/Search/SearchServlet.java
+++ searchServer/src/main/java/de/metalcon/searchServer/Search/SearchServlet.java
@@ -3,7 +3,6 @@ package de.metalcon.searchServer.Search;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,7 +13,7 @@ public class SearchServlet extends HttpServlet {
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+        throws IOException {
         response.setContentType("application/json");
         
         PrintWriter out = response.getWriter();
