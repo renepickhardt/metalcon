84dfdc10db9a817bd0d1d4ce71e8b6c145fdf4af
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2013-08-19 17:28:04 +0200

    Now returning the correct JSON mime type.

--- searchServer/src/main/java/de/metalcon/searchServer/Search/SearchServlet.java
+++ searchServer/src/main/java/de/metalcon/searchServer/Search/SearchServlet.java
@@ -15,6 +15,8 @@ public class SearchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
+        response.setContentType("application/json");
+        
         PrintWriter out = response.getWriter();
         out.println("I'm the searchServer.");
         out.flush();
