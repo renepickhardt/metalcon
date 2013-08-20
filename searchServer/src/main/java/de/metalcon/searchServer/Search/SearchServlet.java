148d33b0747e4d6f8888013ebc5bcce6c739aec9
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2013-08-20 16:10:39 +0200

    Now returning basic JSON.

--- searchServer/src/main/java/de/metalcon/searchServer/Search/SearchServlet.java
+++ searchServer/src/main/java/de/metalcon/searchServer/Search/SearchServlet.java
@@ -7,18 +7,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class SearchServlet extends HttpServlet {
 
     final private static long serialVersionUID = 1L;
     
+    // To avoid warnings with JSONObject.put(). There is no other way to avoid
+    // warnings with JSON-simple library. See:
+    // http://stackoverflow.com/questions/16415436/json-and-generics-in-java-type-safety-warning
+    @SuppressWarnings("unchecked")
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException {
         response.setContentType("application/json");
         
+        String    spellcheck          = "";
+        JSONArray internSearchResults = new JSONArray();
+        JSONArray externSearchResults = new JSONArray();
         JSONObject result = new JSONObject();
+        result.put("spellcheck", spellcheck);
+        result.put("internSearchResults", internSearchResults);
+        result.put("externSearchResults", externSearchResults);
         
         PrintWriter out = response.getWriter();
         out.print(result);
