a40d4374043414edc6016b78aeac868436894a62
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2013-08-20 16:48:01 +0200

    Now accepting query string in q-GET-Parameter. Also now respecting order of JSON attribute declaration.

--- searchServer/src/main/java/de/metalcon/searchServer/Search/SearchServlet.java
+++ searchServer/src/main/java/de/metalcon/searchServer/Search/SearchServlet.java
@@ -2,37 +2,46 @@ package de.metalcon.searchServer.Search;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 public class SearchServlet extends HttpServlet {
 
     final private static long serialVersionUID = 1L;
     
-    // To avoid warnings with JSONObject.put(). There is no other way to avoid
-    // warnings with JSON-simple library. See:
-    // http://stackoverflow.com/questions/16415436/json-and-generics-in-java-type-safety-warning
-    @SuppressWarnings("unchecked")
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException {
+        Map<String, Object> result = new LinkedHashMap<String, Object>();
+        
+        String getQ = request.getParameter("q");
+        if (getQ == null) {
+            result.put("msg", "No query string.");
+        } else {
+            Map<String, Object> resultRequest = new LinkedHashMap<String, Object>();
+            resultRequest.put("q", getQ);
+            
+            String spellcheck = "";
+            List<Object> internSearchResults = new LinkedList<Object>();
+            List<Object> externSearchResults = new LinkedList<Object>();
+            
+            result.put("request", resultRequest);
+            result.put("spellcheck", spellcheck);
+            result.put("internSearchResults", internSearchResults);
+            result.put("externSearchResults", externSearchResults);
+        }
+        
         response.setContentType("application/json");
-        
-        String    spellcheck          = "";
-        JSONArray internSearchResults = new JSONArray();
-        JSONArray externSearchResults = new JSONArray();
-        JSONObject result = new JSONObject();
-        result.put("spellcheck", spellcheck);
-        result.put("internSearchResults", internSearchResults);
-        result.put("externSearchResults", externSearchResults);
-        
         PrintWriter out = response.getWriter();
-        out.print(result);
+        out.print(JSONValue.toJSONString(result));
         out.flush();
     }
     
