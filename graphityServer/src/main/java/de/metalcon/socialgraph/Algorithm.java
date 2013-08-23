c33dceb6050404e2d6a9b9681fba74542ba0b746
Rene Pickhardt <rene@rene-pickhardt.de>
2013-08-23 13:01:39 +0200

    changed benchmark and also the read optimized and write optimized fields

--- graphityServer/src/main/java/de/metalcon/socialgraph/Algorithm.java
+++ graphityServer/src/main/java/de/metalcon/socialgraph/Algorithm.java
@@ -11,11 +11,11 @@ public class Algorithm {
 	/**
 	 * social graph algorithm 'Graphity' by René Pickhardt
 	 */
-	public static final String ReadOptimizedGraphity = "graphity";
+	public static final String ReadOptimizedGraphity = "readoptimized";
 
 	/**
 	 * social graph algorithm 'Graphity' by René Pickhardt (write optimized)
 	 */
-	public static final String WriteOptimizedGraphity = "baseline";
+	public static final String WriteOptimizedGraphity = "writeoptimized";
 
 }
