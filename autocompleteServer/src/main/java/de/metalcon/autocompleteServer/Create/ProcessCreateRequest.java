182bec9a1bfb4a315b0243af1a0ea2aa87ed78fe
CSchowalter <cschowalter@uni-koblenz.de>
2013-08-24 20:27:56 +0200

    Minor refactoring: Removed some useless comments, size of byte-array is no longer hard-coded.

--- autocompleteServer/src/main/java/de/metalcon/autocompleteServer/Create/ProcessCreateRequest.java
+++ autocompleteServer/src/main/java/de/metalcon/autocompleteServer/Create/ProcessCreateRequest.java
@@ -34,11 +34,10 @@ public class ProcessCreateRequest {
 		try {
 			items = FormItemList.extractFormItems(request, factory);
 		} catch (FileUploadException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		// items is == null, when request is not multipart
+		// items is == null, when request Content-Type is not multipart
 		if (items == null) {
 			response.addHttpRequestError(CreateStatusCodes.REQUEST_MUST_BE_MULTIPART);
 			// TODO double-check if return correct
@@ -66,12 +65,10 @@ public class ProcessCreateRequest {
 				suggestionString);
 
 		String indexName = checkIndexName(items, response);
-		// response.addIndexToContainer(indexName);
 		suggestTreeCreateRequestContainer.getComponents().setIndexName(
 				indexName);
 
 		String suggestionKey = checkSuggestionKey(items, response);
-		// response.addSuggestionKeyToContainer(suggestionKey);
 		suggestTreeCreateRequestContainer.getComponents().setKey(suggestionKey);
 
 		Integer weight = checkWeight(items, response);
@@ -79,7 +76,6 @@ public class ProcessCreateRequest {
 			suggestTreeCreateRequestContainer = null;
 			return response;
 		}
-		// response.addWeightToContainer(weight);
 		suggestTreeCreateRequestContainer.getComponents().setWeight(weight);
 
 		// Protocol forbids images for suggestions without keys
@@ -117,13 +113,13 @@ public class ProcessCreateRequest {
 			return null;
 		}
 
-		// TODO replace hard-coded value with file size of imageFile objects
-		byte[] buffer = new byte[50000];
+		// TODO find better approximation for resulting file size (after Base64
+		// encoding)
+		byte[] buffer = new byte[(int) imageFile.getSize() * 3 / 2];
 		byte[] tmp = null;
 		try {
 			int size = imageFile.getInputStream().read(buffer);
 			if (size > 0) {
-				// System.out.println(buffer);
 				tmp = new byte[size];
 				for (int i = 0; i < size; i++) {
 					tmp[i] = buffer[i];
@@ -141,58 +137,6 @@ public class ProcessCreateRequest {
 			return null;
 		}
 
-		// BufferedImage bufferedImage = null;
-		// FileReader fileReader = null;
-		// try {
-		// fileReader = new FileReader(imageFile);
-		// } catch (IOException e2) {
-		// // TODO Auto-generated catch block
-		// e2.printStackTrace();
-		// return null;
-		// }
-		// maybe there is a better way for verifying image encoding than MIME?
-		// String mimeType = new
-		// MimetypesFileTypeMap().getContentType(imageFile);
-		// if (!(mimeType.equals("image/JPEG"))) {
-		// try {
-		// fileReader.close();
-		// } catch (IOException e) {
-		// e.printStackTrace();
-		// }
-		// return null;
-		// }
-		//
-		// long fileLength = imageFile.length();
-		// char[] cbuf = null;
-		// try {
-		// fileReader.read(cbuf, 0, (int) fileLength);
-		// fileReader.close();
-		// } catch (IOException e1) {
-		// e1.printStackTrace();
-		// return null;
-		// }
-		// byte[] rawImageData = new byte[(int) fileLength];
-		// for (int i = 0; i < fileLength; i++) {
-		// rawImageData[i] = (byte) cbuf[i];
-		// }
-		// byte[] base64EncodedImage = Base64.encodeBase64(rawImageData);
-		// String result = new String(base64EncodedImage);
-		//
-		// try {
-		// ByteArrayInputStream bais = new ByteArrayInputStream(rawImageData);
-		// bufferedImage = ImageIO.read(bais);
-		// // TODO: maybe less strong
-		// if ((ProtocolConstants.IMAGE_WIDTH < bufferedImage.getWidth()) ||
-		// (ProtocolConstants.IMAGE_HEIGHT < bufferedImage
-		// .getHeight())) {
-		// return null;
-		// }
-		//
-		// } catch (IOException e) {
-		// e.printStackTrace();
-		// return null;
-		// }
-		System.out.println(imageB64);
 		return "data:image/jpg;base64," + imageB64;
 	}
 
@@ -202,16 +146,13 @@ public class ProcessCreateRequest {
 		try {
 			weight = items.getField(ProtocolConstants.SUGGESTION_WEIGHT);
 		} catch (IllegalArgumentException e) {
-			// TODO: RefactorName
 			response.addWeightNotGivenError(CreateStatusCodes.WEIGHT_NOT_GIVEN);
 			return null;
 		}
 
 		try {
 			return Integer.parseInt(weight);
-		}
-		// TODO: verify this is the right exception
-		catch (NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			response.addWeightNotANumberError(CreateStatusCodes.WEIGHT_NOT_A_NUMBER);
 			return null;
 		}
@@ -236,7 +177,6 @@ public class ProcessCreateRequest {
 			key = items.getField(ProtocolConstants.SUGGESTION_KEY);
 		}
 
-		// this exception is no reason to abort processing!
 		catch (IllegalArgumentException e) {
 			response.addNoKeyWarning(CreateStatusCodes.SUGGESTION_KEY_NOT_GIVEN);
 			return null;
@@ -252,16 +192,12 @@ public class ProcessCreateRequest {
 	private static String checkSuggestionString(FormItemList items,
 			ProcessCreateResponse response) {
 		String suggestString = null;
-		// check is the field exists
 		try {
 			suggestString = items.getField(ProtocolConstants.SUGGESTION_STRING);
 		} catch (IllegalArgumentException e) {
-			// TODO: RefactorName
 			response.addQueryNameMissingError(CreateStatusCodes.QUERYNAME_NOT_GIVEN);
 			return null;
 		}
-		// checks if the suggestion String length matches ASTP requirements
-		// FIXME: JUnit says the Error-Message is not added
 		if (suggestString.length() > ProtocolConstants.MAX_SUGGESTION_LENGTH) {
 			response.addQueryNameTooLongError(CreateStatusCodes.SUGGESTION_STRING_TOO_LONG);
 			return null;
