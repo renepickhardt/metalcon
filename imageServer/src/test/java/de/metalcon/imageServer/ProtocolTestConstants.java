286b8289b13e315f97173a234907b86fe74087c8
CSchowalter <cschowalter@uni-koblenz.de>
2013-09-02 17:01:44 +0200

    created test structure

---
+++ imageServer/src/test/java/de/metalcon/imageServer/ProtocolTestConstants.java
@@ -0,0 +1,12 @@
+package de.metalcon.imageServer;
+
+
+public class ProtocolTestConstants {
+
+	// TODO: insert valid constants
+	// public static final String VALID_IMAGE = null;
+
+	// public final InputStream imageStream;
+	// public final String metaData;
+	// public final String autoRotate;
+}
