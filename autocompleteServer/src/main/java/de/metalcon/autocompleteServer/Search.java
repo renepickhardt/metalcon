050b49f7a86fae06a358e3758745766b4481bac7
CSchowalter <cschowalter@uni-koblenz.de>
2013-09-02 15:33:34 +0200

    created test folder

--- autocompleteServer/src/main/java/de/metalcon/autocompleteServer/Search.java
+++ autocompleteServer/src/main/java/de/metalcon/autocompleteServer/Search.java
@@ -15,20 +15,6 @@ import de.metalcon.autocompleteServer.Helper.ProtocolConstants;
 import de.metalcon.autocompleteServer.Helper.SuggestTree;
 
 public class Search {
-	// public static SuggestTree suggestTree = null;
-
-	// public static String treeSearch(String input) {
-	// if (suggestTree == null) { //When the ContextListener works as intended,
-	// the initalization should already be done before calling this method
-	// initilizeSuggestTree();
-	// }
-	// Node result = suggestTree.getBestSuggestions(input);
-	// StringBuffer resultList = new StringBuffer("");
-	// for (int i = 0; i < result.listLength(); ++i) {
-	// resultList = resultList.append(result.getSuggestion(i) + ",");
-	// }
-	// return resultList.toString();
-	// }
 
 	public static void initilizeSuggestTree(ServletContext context) {
 		SuggestTree suggestTree = new SuggestTree(
