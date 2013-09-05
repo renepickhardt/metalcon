5d997dc3d8be15347af888ccc49162dad8196544
Patrik Schmidt <patrik@visionhack.de>
2013-09-05 18:11:53 +0200

    rewrite putEdge method

--- haveInCommons/src/main/java/de/metalcon/haveInCommons/LuceneRead.java
+++ haveInCommons/src/main/java/de/metalcon/haveInCommons/LuceneRead.java
@@ -14,11 +14,11 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
-import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
@@ -34,6 +34,9 @@ public class LuceneRead implements HaveInCommons {
 	protected IndexWriter iw;
 	protected StandardAnalyzer analyzer;
 	protected IndexReader ir;
+	protected IndexWriterConfig iwc;
+	protected Directory dir;
+
 	private static final String INDEX_DIRECTORY = "luceneBenchmark";
 
 	/**
@@ -41,17 +44,16 @@ public class LuceneRead implements HaveInCommons {
 	 * 
 	 */
 	public LuceneRead() throws IOException {
-		Directory dir = FSDirectory.open(new File(INDEX_DIRECTORY));
+		this.dir = FSDirectory.open(new File(INDEX_DIRECTORY));
 		this.analyzer = new StandardAnalyzer(Version.LUCENE_40);
-		IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_40,
-				this.analyzer);
+		this.iwc = new IndexWriterConfig(Version.LUCENE_40, this.analyzer);
 
 		if (true) {
-			iwc.setOpenMode(OpenMode.CREATE);
+			this.iwc.setOpenMode(OpenMode.CREATE);
 		} else {
-			iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+			this.iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
 		}
-		this.iw = new IndexWriter(dir, iwc);
+		this.iw = new IndexWriter(this.dir, this.iwc);
 		try {
 			this.ir = DirectoryReader.open(this.iw.getDirectory());
 		} catch (IOException e) {
@@ -78,34 +80,17 @@ public class LuceneRead implements HaveInCommons {
 	 */
 	@Override
 	public Set<String> getCommonNodes(String uuid1, String uuid2) {
-		Term term = new Term("nodeID", uuid1);
-		Query query = new TermQuery(term);
+
+		Query query1 = new TermQuery(new Term("id", uuid1));
+		Query query2 = new TermQuery(new Term("id", uuid2));
+
+		BooleanQuery bQuery = new BooleanQuery();
+		bQuery.add(query1, BooleanClause.Occur.MUST);
+
 		TopDocs tdocs = null;
 		IndexSearcher indexSearcher = new IndexSearcher(this.ir);
 
-		try {
-			tdocs = indexSearcher.search(query, 1);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		Set<String> items = new HashSet<String>();
-		// TODO: Make assumptions about run-time complexity
-		if (tdocs.totalHits < 0) {
-			ScoreDoc[] scoreDocsArray = tdocs.scoreDocs;
-			for (ScoreDoc scoredoc : scoreDocsArray) {
-				Document doc = null;
-				try {
-					doc = indexSearcher.doc(scoredoc.doc);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				IndexableField[] fields = doc.getFields("nodeId");
-				for (IndexableField field : fields) {
-					items.add(field.stringValue());
-				}
-			}
-		}
 
 		return items;
 	}
@@ -118,103 +103,29 @@ public class LuceneRead implements HaveInCommons {
 	 */
 	@Override
 	public void putEdge(String from, String to) {
-		// TODO Auto-generated method stub
-
-		// first, search if document with nodeId already exists
-
-		// search for node "from"
-		IndexReader reader = null;
-		TopDocs tdocs = null;
 		try {
-			reader = DirectoryReader.open(this.iw.getDirectory());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			this.iw = new IndexWriter(this.dir, this.iwc);
+			this.ir = DirectoryReader.open(this.iw.getDirectory());
 
-		IndexSearcher indexSearcher = new IndexSearcher(reader);
-		Term term = new Term("nodeId", from);
-		Query query = new TermQuery(term);
+			Document doc = new Document();
+			Field field = new StringField("neighbour", to, Field.Store.YES);
+			doc.add(field);
 
-		try {
-			// TODO: Can we assume that every node is unique? Otherwise we have
-			// to obtain more results
-			tdocs = indexSearcher.search(query, 1);
+			// write into "from"-node
+			this.iw.updateDocument(new Term("id", from), doc);
+
+			doc = new Document();
+			field = new StringField("neighbour", from, Field.Store.YES);
+			doc.add(field);
+
+			// write into "to"-node
+			this.iw.updateDocument(new Term("id", to), doc);
+
+			this.iw.close();
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			tdocs = null;
-		}
-
-		// obtain the document and insert new "to" node, otherwise add a new
-		// document
-		if (tdocs != null && tdocs.totalHits != 0) {
-			ScoreDoc[] scoreDocsArray = tdocs.scoreDocs;
-			for (ScoreDoc scoredoc : scoreDocsArray) {
-				Document doc = null;
-				try {
-					doc = indexSearcher.doc(scoredoc.doc);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				Field pathfield = new StringField("nodeId", to, Field.Store.YES);
-				doc.add(pathfield);
-			}
-		} else {
-			Document doc = new Document();
-			Field pathfield = new StringField("nodeId", to, Field.Store.YES);
-			doc.add(pathfield);
-			try {
-				this.iw.addDocument(doc);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-		// search for node "to"
-		reader = null;
-		tdocs = null;
-
-		term = new Term("nodeId", to);
-		query = new TermQuery(term);
-
-		try {
-			// TODO: Can we assume that every node is unique? Otherwise we have
-			// to obtain more results
-			tdocs = indexSearcher.search(query, 1);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			tdocs = null;
-		}
-
-		// obtain the document and insert new "to" node, otherwise add a new
-		// document
-		if (tdocs != null && tdocs.totalHits != 0) {
-			ScoreDoc[] scoreDocsArray = tdocs.scoreDocs;
-			for (ScoreDoc scoredoc : scoreDocsArray) {
-				Document doc = null;
-				try {
-					doc = indexSearcher.doc(scoredoc.doc);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				Field pathfield = new StringField("nodeId", from,
-						Field.Store.YES);
-				doc.add(pathfield);
-			}
-		} else {
-			Document doc = new Document();
-			Field pathfield = new StringField("nodeId", from, Field.Store.YES);
-			doc.add(pathfield);
-			try {
-				this.iw.addDocument(doc);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 		}
 
 	}
