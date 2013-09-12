8d5110622f01992485838534a3afecde21e95989
CSchowalter <cschowalter@uni-koblenz.de>
2013-09-12 17:26:38 +0200

    added test classes according to new protocol structure

---
+++ imageServer/src/test/java/de/metalcon/imageServer/TestReadBulkRequest.java
@@ -0,0 +1,14 @@
+package de.metalcon.imageServer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestReadBulkRequest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ imageServer/src/test/java/de/metalcon/imageServer/TestReadScaledRequest.java
@@ -0,0 +1,14 @@
+package de.metalcon.imageServer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestReadScaledRequest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
