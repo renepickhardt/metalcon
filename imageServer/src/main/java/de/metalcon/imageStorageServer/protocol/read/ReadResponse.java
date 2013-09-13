cbb2260a8831d24b43df4622581c443da4959037
CSchowalter <cschowalter@uni-koblenz.de>
2013-09-13 19:32:25 +0200

    added Javadocs

--- imageServer/src/main/java/de/metalcon/imageStorageServer/protocol/read/ReadResponse.java
+++ imageServer/src/main/java/de/metalcon/imageStorageServer/protocol/read/ReadResponse.java
@@ -10,6 +10,10 @@ public class ReadResponse extends Response {
 	// TODO: refactor code using constants (in malformed-error methods)
 	// TODO: refactor to match the other protocol request/response patterns
 
+	/**
+	 * Adds an error message for a missing image identifier parameter to the
+	 * response.
+	 */
 	public void addNoImageIdentifierError() {
 		this.parameterMissing(
 				ProtocolConstants.Parameters.Read.IMAGE_IDENTIFIER,
@@ -17,6 +21,10 @@ public class ReadResponse extends Response {
 
 	}
 
+	/**
+	 * Adds an error message for a malformed image identifier String to the
+	 * response.
+	 */
 	public void addOriginalImageFlagMalformedError() {
 		this.addStatusMessage(
 				"request corrupt: parameter \"originalFlag\" is malformed",
@@ -24,24 +32,30 @@ public class ReadResponse extends Response {
 
 	}
 
-	public void addNoOriginalFlagError() {
-		this.parameterMissing(ProtocolConstants.Parameters.Read.ORIGINAL_FLAG,
-				"The originalImage flag is missing. Please deliver one");
-
-	}
-
+	/**
+	 * Adds an error message for missing image scaling width parameter to the
+	 * response.
+	 */
 	public void addNoImageWidthError() {
 		this.parameterMissing(
 				ProtocolConstants.Parameters.Read.IMAGE_WIDTH,
 				"The image width is not given. Either deliver width AND height or leave them out and ask for the unscaled image");
 	}
 
+	/**
+	 * Adds an error message for a missing image scaling height parameter to the
+	 * response.
+	 */
 	public void addNoImageHeightError() {
 		this.parameterMissing(
 				ProtocolConstants.Parameters.Read.IMAGE_HEIGHT,
 				"The image height is not given. Either deliver width AND height or leave them out and ask for the unscaled image");
 	}
 
+	/**
+	 * Adds an error message for a malformed image scaling width parameter
+	 * string to the response.
+	 */
 	public void addImageWidthMalformedError() {
 		this.addStatusMessage(
 				"request corrupt: parameter \"imageWidth\" is malformed",
@@ -49,6 +63,10 @@ public class ReadResponse extends Response {
 
 	}
 
+	/**
+	 * Adds an error message for a malformed image scaling height parameter
+	 * string to the response.
+	 */
 	public void addImageHeightMalformedError() {
 		this.addStatusMessage(
 				"request corrupt: parameter \"imageHeight\" is malformed",
@@ -56,11 +74,18 @@ public class ReadResponse extends Response {
 
 	}
 
+	/**
+	 * Adds an image not founf error message to the response.
+	 */
 	public void addImageNotFoundError() {
 		this.addStatusMessage(
 				ProtocolConstants.StatusMessage.Read.NO_IMAGE_FOUND, "");
 	}
 
+	/**
+	 * Adds a warning concerning the requested image size being bigger than the
+	 * available source image.
+	 */
 	public void addGeometryBiggerThanOriginalWarning() {
 		this.addStatusMessage(
 				ProtocolConstants.StatusMessage.Read.GEOMETRY_BIGGER_THAN_ORIGINAL,
