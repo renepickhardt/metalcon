9303652590743e01b2dcc096bd7dd81c0d79d9c4
CSchowalter <cschowalter@uni-koblenz.de>
2013-09-23 19:09:52 +0200

    added class for create from url request test

---
+++ imageServer/src/test/java/de/metalcon/imageServer/protocol/CreateFromUrlTest.java
@@ -0,0 +1,16 @@
+package de.metalcon.imageServer.protocol;
+
+import static org.junit.Assert.fail;
+
+public class CreateFromUrlTest extends RequestTest {
+
+	// TODO implement test
+	public void testMalformedUrl() {
+		fail("not yet implemented!");
+	}
+
+	// TODO implement test
+	public void testurlMissing() {
+		fail("not yet implemented!");
+	}
+}
