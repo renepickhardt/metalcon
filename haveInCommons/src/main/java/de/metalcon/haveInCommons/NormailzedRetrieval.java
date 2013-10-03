6a4839ed35cd8b79e9700d480b3440c99ee32bc2
Jonas Kunze <jonas@metalcon.de>
2013-10-03 19:32:03 +0200

    Typo fixed

--- haveInCommons/src/main/java/de/metalcon/haveInCommons/NormailzedRetrieval.java
+++ haveInCommons/src/main/java/de/metalcon/haveInCommons/NormailzedRetrieval.java
@@ -19,14 +19,15 @@ import java.util.*;
 public class NormailzedRetrieval implements HaveInCommons {
 	protected Graph graph;
 
-    protected HashMap<Long, Vertex> vertexIndex;
-    protected HashMap<Vertex, Long> reverseVertexIndex;
+	protected HashMap<Long, Vertex> vertexIndex;
+	protected HashMap<Vertex, Long> reverseVertexIndex;
+
 	/**
 	 * constructor
 	 */
 	public NormailzedRetrieval() {
 		graph = new DirectedSparseGraph();
-        vertexIndex = new HashMap<Long,Vertex>();
+		vertexIndex = new HashMap<Long, Vertex>();
 	}
 
 	/**
@@ -37,22 +38,22 @@ public class NormailzedRetrieval implements HaveInCommons {
 	 */
 	protected boolean storeEdge(long from, long to) {
 
-        Vertex f = vertexIndex.get(from);
+		Vertex f = vertexIndex.get(from);
 		if (f == null) {
 			f = new DirectedSparseVertex();
 
-            vertexIndex.put(from, f);
-            reverseVertexIndex.put(f,from);
+			vertexIndex.put(from, f);
+			reverseVertexIndex.put(f, from);
 
-            graph.addVertex(f);
+			graph.addVertex(f);
 		}
 
-        Vertex t = vertexIndex.get(to);
+		Vertex t = vertexIndex.get(to);
 		if (t == null) {
 			t = new DirectedSparseVertex();
 
 			vertexIndex.put(to, t);
-            reverseVertexIndex.put(t,to);
+			reverseVertexIndex.put(t, to);
 
 			graph.addVertex(t);
 		}
@@ -65,13 +66,12 @@ public class NormailzedRetrieval implements HaveInCommons {
 			try {
 				graph.addEdge(edge);
 			} catch (Exception e) {
-				//System.out.println("could not add edge");
+				// System.out.println("could not add edge");
 				return false;
 			}
 		}
 		return true;
 	}
-
 
 	/*
 	 * (non-Javadoc)
@@ -84,13 +84,13 @@ public class NormailzedRetrieval implements HaveInCommons {
 		Vertex f = vertexIndex.get(uuid1);
 		Vertex t = vertexIndex.get(uuid2);
 		Set<Vertex> intersection = new HashSet<Vertex>(f.getOutEdges());
-        intersection.retainAll(t.getInEdges());
-        long[] result = new long[intersection.size()];
-        int i = 0;
-        for (Vertex v: intersection) {
-            result[i++] = reverseVertexIndex.get(v).longValue() ;
-        }
-        return result;
+		intersection.retainAll(t.getInEdges());
+		long[] result = new long[intersection.size()];
+		int i = 0;
+		for (Vertex v : intersection) {
+			result[i++] = reverseVertexIndex.get(v).longValue();
+		}
+		return result;
 	}
 
 	/*
@@ -101,7 +101,7 @@ public class NormailzedRetrieval implements HaveInCommons {
 	 */
 	public void putEdge(long from, long to) {
 		if (storeEdge(from, to)) {
-//			updateCommons(from, to);
+			// updateCommons(from, to);
 		}
 	}
 
@@ -111,9 +111,7 @@ public class NormailzedRetrieval implements HaveInCommons {
 	 * @see de.metalcon.haveInCommons.HaveInCommons#delegeEdge(java.lang.String,
 	 * java.lang.String)
 	 */
-	public boolean delegeEdge(long from, long to) {
+	public boolean deleteEdge(long from, long to) {
 		return false;
 	}
-
-
 }
