d7b45b27aa89c75247cd7c0b285708c4d1ffa5ac
Heinrich Hartmann <hartmann@uni-koblenz.de>
2013-10-03 22:37:19 +0200

    Repaired Neo4J Persistence Like.

--- haveInCommons/src/main/java/de/metalcon/haveInCommons/PersistentReadOptimized.java
+++ haveInCommons/src/main/java/de/metalcon/haveInCommons/PersistentReadOptimized.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package de.metalcon.haveInCommons;
 
@@ -19,157 +19,155 @@ import org.neo4j.kernel.EmbeddedGraphDatabase;
 
 /**
  * @author Rene Pickhardt
- * 
  */
 public class PersistentReadOptimized implements HaveInCommons {
-	private GraphDatabaseService graphDB;
+    private GraphDatabaseService graphDB;
+    Index<Node> ix;
+    Index<Relationship> relIndex;
 
-	/**
-	 * 
-	 */
-	public PersistentReadOptimized() {
-		graphDB = new EmbeddedGraphDatabase("/tmp/Neo4j");
-	}
+    /**
+     *
+     */
+    public PersistentReadOptimized() {
+        graphDB = new EmbeddedGraphDatabase("/home/hartmann/LikeButtonNeo4j");
+        ix = graphDB.index().forNodes("nodes");
+        relIndex = graphDB.index().forRelationships("edges");
+    }
 
-	private void updateCommons(Node from, Node to) {
-		if (from == null || to == null)
-			throw new IllegalArgumentException();
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * de.metalcon.haveInCommons.HaveInCommons#getCommonNodes(java.lang.String,
+     * java.lang.String)
+     */
+    @Override
+    public long[] getCommonNodes(long uuid1, long uuid2) {
+        Node from = ix.get("id", uuid1).getSingle();
+        Node to = ix.get("id", uuid2).getSingle();
 
-		String fromId = (String) from.getProperty("id");
-		for (Relationship r : to.getRelationships(Direction.BOTH)) {
-			Node dest = r.getEndNode();
-			String id = (String) dest.getProperty("id");
-			addCommonNode(to, fromId, id);
-		}
+        if (to == null || from == null)
+            return null;
 
-		String toId = (String) to.getProperty("id");
-		for (Relationship r : from.getRelationships(Direction.BOTH)) {
-			Node src = r.getStartNode();
-			String id = (String) src.getProperty("id");
-			addCommonNode(from, id, toId);
-		}
-	}
+        Set<String> s = new HashSet<String>();
+        ArrayList<Long> res = new ArrayList<Long>();
+        for (Relationship r : from.getRelationships()) {
+            Node tmp = r.getOtherNode(from);
+            s.add((String) tmp.getProperty("id"));
+        }
 
-	/**
-	 * @param to
-	 * @param fromId
-	 * @param id
-	 */
-	private void addCommonNode(Node n, String from, String to) {
-		Index<Node> ix = graphDB.index().forNodes("commons");
-		ix.add(n, "key", from + ":" + to);
-	}
+        for (Relationship r : to.getRelationships()) {
+            Node tmp = r.getOtherNode(to);
+            long key = (long) tmp.getProperty("id");
+            if (s.contains(key))
+                res.add(key);
+        }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * de.metalcon.haveInCommons.HaveInCommons#getCommonNodes(java.lang.String,
-	 * java.lang.String)
-	 */
-	@Override
-	public long[] getCommonNodes(long uuid1, long uuid2) {
-		// Index<Node> ix = graphDB.index().forNodes("commons");
-		// //IndexHits<Node> hits = ix.query("key", new QueryContext("*"));
-		// IndexHits<Node> hits = ix.get("key", uuid1 + "\\:" + uuid2);
-		Index<Node> ix = graphDB.index().forNodes("nodes");
-		Node from = ix.get("id", uuid1).getSingle();
-		Node to = ix.get("id", uuid2).getSingle();
-		if (to == null || from == null)
-			return null;
-		Set<String> s = new HashSet<String>();
-		ArrayList<Long> res = new ArrayList<Long>();
-		for (Relationship r : from.getRelationships()) {
-			Node tmp = r.getOtherNode(from);
-			s.add((String) tmp.getProperty("id"));
-		}
-		for (Relationship r : to.getRelationships()) {
-			Node tmp = r.getOtherNode(to);
-			long key = (long) tmp.getProperty("id");
-			if (s.contains(key))
-				res.add(key);
-		}
-		return toPrimitive(res);
-	}
+        return toPrimitive(res);
+    }
 
 	/*
-	 * (non-Javadoc)
+     * (non-Javadoc)
 	 * 
 	 * @see de.metalcon.haveInCommons.HaveInCommons#putEdge(java.lang.String,
 	 * java.lang.String)
 	 */
-	@Override
-	public void putEdge(long from, long to) {
-		Transaction tx = graphDB.beginTx();
-		try {
-			storeEdge(from, to);
-			tx.success();
-		} catch (Exception e) {
-			e.printStackTrace();
-			tx.failure();
-			System.exit(-1);
-			return;
-		} finally {
-			tx.finish();
-		}
-	}
 
-	/**
-	 * @param from
-	 * @param to
-	 * @return
-	 */
-	private void storeEdge(long from, long to) {
-		Node f = null;
-		Node t = null;
-		Index<Node> nodeIndex = graphDB.index().forNodes("nodes");
-		f = nodeIndex.get("id", from).getSingle();
-		if (f == null) {
-			f = graphDB.createNode();
-			f.setProperty("id", from);
-			nodeIndex.add(f, "id", from);
-		}
-		t = nodeIndex.get("id", to).getSingle();
-		if (t == null) {
-			t = graphDB.createNode();
-			t.setProperty("id", to);
-			nodeIndex.add(t, "id", to);
-		}
 
-		Index<Relationship> relIndex = graphDB.index()
-				.forRelationships("edges");
-		Relationship r = null;
-		r = relIndex.get("id", from + ":" + to).getSingle();
-		if (r == null) {
-			r = f.createRelationshipTo(t,
-					DynamicRelationshipType.withName("follows"));
-			relIndex.add(r, "id", from + ":" + to);
-		}
-		// updateCommons(f, t);
-	}
+    ArrayList<Long> putBuffer_1 = new ArrayList<Long>();
+    ArrayList<Long> putBuffer_2 = new ArrayList<Long>();
+    static final int bufSize = 5000;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see de.metalcon.haveInCommons.HaveInCommons#delegeEdge(java.lang.String,
-	 * java.lang.String)
-	 */
-	@Override
-	public boolean deleteEdge(long from, long to) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public void putEdge(long from, long to) {
+        if (putBuffer_1.size() < bufSize) {
+            putBuffer_1.add(from);
+            putBuffer_2.add(to);
+        } else {
+            Transaction tx = graphDB.beginTx();
 
-	/*
-	 * Converts a List<Long> to an array of primitive longs
-	 */
-	protected long[] toPrimitive(List<Long> list) {
-		long[] ints = new long[list.size()];
-		int i = 0;
-		for (Long n : list) {
-			ints[i++] = n;
-		}
-		return ints;
-	}
+            try {
+
+                for (int i = 0; i < bufSize; i++) {
+
+                    storeEdge(putBuffer_1.get(i), putBuffer_2.get(i));
+
+                }
+
+                tx.success();
+
+                putBuffer_1.clear();
+                putBuffer_2.clear();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                tx.failure();
+                System.exit(-1);
+                return;
+            } finally {
+                tx.finish();
+            }
+        }
+
+
+    }
+
+    /**
+     * @param from
+     * @param to
+     * @return
+     */
+    private void storeEdge(long from, long to) {
+        Node f = null;
+        Node t = null;
+        Index<Node> nodeIndex = graphDB.index().forNodes("nodes");
+
+        f = nodeIndex.get("id", from).getSingle();
+        if (f == null) {
+            f = graphDB.createNode();
+            f.setProperty("id", from);
+            nodeIndex.add(f, "id", from);
+        }
+
+        t = nodeIndex.get("id", to).getSingle();
+        if (t == null) {
+            t = graphDB.createNode();
+            t.setProperty("id", to);
+            nodeIndex.add(t, "id", to);
+        }
+
+
+        Relationship r = relIndex.get("id", from + ":" + to).getSingle();
+        if (r == null) {
+            r = f.createRelationshipTo(t,
+                    DynamicRelationshipType.withName("follows"));
+            relIndex.add(r, "id", from + ":" + to);
+        }
+
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see de.metalcon.haveInCommons.HaveInCommons#delegeEdge(java.lang.String,
+     * java.lang.String)
+     */
+    @Override
+    public boolean deleteEdge(long from, long to) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /*
+     * Converts a List<Long> to an array of primitive longs
+     */
+    protected long[] toPrimitive(List<Long> list) {
+        long[] ints = new long[list.size()];
+        int i = 0;
+        for (Long n : list) {
+            ints[i++] = n;
+        }
+        return ints;
+    }
 
 }
