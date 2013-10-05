206d206fc35ad28f92862619f5a0dd6630f2728d
Jonas Kunze <jonas@metalcon.de>
2013-10-05 21:23:39 +0200

    Node scaffolding and Like Class implemented

--- haveInCommons/src/main/java/de/metalcon/like/Node.java
+++ haveInCommons/src/main/java/de/metalcon/like/Node.java
@@ -6,10 +6,41 @@ import java.io.File;
  * @author Jonas Kunze
  */
 public class Node {
+	private static final int LastLikeCacheSize = 10;
+
 	private final long ID;
 	private final File LikeListFile;
-	private long[] lastLikes;
+	private Like[] lastLikes = new Like[LastLikeCacheSize];
 	private final File PersistentLikesFile;
 	private int lastCacheUpdate;
-	private Node[] firendList;
+	private Node[] friendList;
+
+	public Node(final long ID) {
+		this.ID = ID;
+
+	}
+
+	public Like[] getLikesFromTimeOn(final int timestamp) {
+		return null;
+	}
+
+	public Like[] getDislikesFromTimeOn(final int timestamp) {
+		return null;
+	}
+
+	public boolean addLike(long uuid, int timestamp) {
+		return true;
+	}
+
+	public boolean removeLike(long uuid) {
+		return true;
+	}
+
+	public boolean addFriendship(Node newFriend) {
+		return true;
+	}
+
+	public boolean removeFriendship(Node n) {
+		return true;
+	}
 }
