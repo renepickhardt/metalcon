92ad7202f79a903112e04c4d14d1593e55952ddb
Jonas Kunze <jonas@metalcon.de>
2013-10-15 22:17:58 +0200

    Merged utils projects to one eclipse project

--- utils/src/de/metalcon/utils/UUIDConverter.java
+++ utils/src/de/metalcon/utils/UUIDConverter.java
@@ -9,46 +9,49 @@ package de.metalcon.utils;
 
 /**
  * @author Rene Pickhardt
- *
+ * 
  */
 public class UUIDConverter {
-	private final static char[] tokens =  {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'};
+	private final static char[] tokens = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+			'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+			'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6',
+			'7', '8', '9' };
 	private final static int MAX_TOKEN = 35;
 
 	private final static int[] reverseTokens;
 	static {
 		reverseTokens = new int[256];
-		for (int i = 0;i!=256;++i){
+		for (int i = 0; i != 256; ++i) {
 			reverseTokens[i] = -1;
 		}
-		for (int i = 0; i!= MAX_TOKEN; ++i){
-			reverseTokens[(int)(tokens[i])] = i;
+		for (int i = 0; i != MAX_TOKEN; ++i) {
+			reverseTokens[(int) (tokens[i])] = i;
 		}
 	}
-		
-	public static String serialize(long uuid){
+
+	public static String serialize(long uuid) {
 		StringBuilder string = new StringBuilder(13);
-		for (int i = 0;i!=13;++i){
-			int rest = (int)(uuid%MAX_TOKEN);
+		for (int i = 0; i != 13; ++i) {
+			int rest = (int) (uuid % MAX_TOKEN);
 			string.append(tokens[rest]);
 			uuid = uuid / MAX_TOKEN;
 		}
 		return string.toString();
 	}
-	
+
 	/**
 	 * @param idString
 	 * @return
 	 */
 	public static long deserialize(final String idString) {
 		long tmp = 0;
-		for (int i = idString.length()-1; i >= 0  ; --i){
-			tmp*=MAX_TOKEN;
+		for (int i = idString.length() - 1; i >= 0; --i) {
+			tmp *= MAX_TOKEN;
 			char c = idString.charAt(i);
-			if (reverseTokens[(int)c]==-1){
+			if (reverseTokens[(int) c] == -1) {
 				throw new NumberFormatException();
 			}
-			tmp+=reverseTokens[(int)c];
+			tmp += reverseTokens[(int) c];
 		}
 		return tmp;
 	}
