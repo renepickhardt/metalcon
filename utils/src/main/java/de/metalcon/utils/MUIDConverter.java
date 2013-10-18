edb8af09a90a97d709b2dd5a481fcbc9e1bd2bf5
Jonas Kunze <jonas@metalcon.de>
2013-10-18 21:38:53 +0200

    MUIDConverter: added getMUID defining the MUID format. Also added getter methods to read type, source, timestamp and ID from a given MUID

--- utils/src/main/java/de/metalcon/utils/MUIDConverter.java
+++ utils/src/main/java/de/metalcon/utils/MUIDConverter.java
@@ -15,10 +15,10 @@ import java.nio.ByteBuffer;
  * 
  */
 public class MUIDConverter {
-	private final static char[] tokens = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-			'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-			'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6',
-			'7', '8', '9' };
+	private final static char[] tokens = { '0', '1', '2', '3', '4', '5', '6',
+			'7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+			'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+			'x', 'y', 'z' };
 	private final static int RADIX = tokens.length;
 	private final static short MUID_LENGTH = 13;
 
@@ -31,6 +31,50 @@ public class MUIDConverter {
 		for (int i = 0; i != RADIX; ++i) {
 			reverseTokens[(int) (tokens[i])] = i;
 		}
+	}
+
+	public static long getMUID(final short type, final byte source,
+			final int timestamp, final short ID) {
+		if (type >= (1 << 9)) {
+			throw new RuntimeException("Type may not be larger or equal to "
+					+ (1 << 9));
+		}
+		if (source >= (1 << 5)) {
+			throw new RuntimeException("Source may not be larger or equal to "
+					+ (1 << 5));
+		}
+
+		return
+		/* Highest bit is 1 for constant length */
+		1l << (64 - 1)
+		/* Highest 10 bits are type */
+		| (((long) type & 511) << (64 - 9 - 1))
+		/*
+		 * Next bit is empty so that the first two alphanumerics only depend on
+		 * type and not also source
+		 */
+		/* Next 5 bits are source */
+		| (((long) source & 31) << (64 - 1 - 9 - 1 - 5))
+		/* Next 4 bytes are TS */
+		| (((long) timestamp & 0xFFFFFFFFL) << (64 - 1 - 9 - 1 - 5 - 32))
+		/* Next 2 bytes are ID */
+		| (short) ID & 0xFFFFL;
+	}
+
+	public static final short getType(final long uuid) {
+		return (short) ((uuid >>> (64 - 10)) & 511);
+	}
+
+	public static final byte getSource(final long uuid) {
+		return (byte) ((uuid >>> (64 - 1 - 9 - 1 - 5)) & 31);
+	}
+
+	public static final int getTimestamp(final long uuid) {
+		return (int) ((uuid >>> (64 - 1 - 9 - 1 - 5 - 32)) & 0xFFFFFFFFL);
+	}
+
+	public static final short getID(final long uuid) {
+		return (short) (uuid & 0xFFFFL);
 	}
 
 	public static final String serialize(long uuid) {
@@ -51,6 +95,9 @@ public class MUIDConverter {
 		// }
 		// return Long.toString(rest, RADIX) + serialize(bottomDigit);
 
+		/*
+		 * Do not use Long.toString to interpret the uuid as unsigned long
+		 */
 		byte[] bytes = ByteBuffer.allocate(8).putLong(uuid).array();
 		return new BigInteger(1, bytes).toString(RADIX);
 	}
--- utils/src/test/java/de/metalcon/utils/MUIDConverterTest.java
+++ utils/src/test/java/de/metalcon/utils/MUIDConverterTest.java
@@ -14,66 +14,109 @@ import org.junit.Test;
  * 
  */
 public class MUIDConverterTest {
-
 	@Test
 	public void performanceTest() {
-		for (long i = 0; i < 15; i += 1) {
-			MUID muid = new MUID((short) i, (byte) 2, 3, (short) 4);
-			System.out.println(i + " : " + muid.toBinaryString() + " : "
-					+ muid.toString());
-		}
-
-		System.exit(1);
-		for (long i = -100; i < 100; i += 10) {
-			String serialized = MUIDConverter.serialize(i);
-			long deserialized = MUIDConverter.deserialize(serialized);
-			System.out.println(i + " : " + serialized + " : " + deserialized
-					+ " : " + serialized.length());
-		}
-
-		// for (long i = Long.MIN_VALUE; i < Long.MAX_VALUE; i += Long.MAX_VALUE
-		// / 100) {
-		// String serialized = UUIDConverter.serialize(i);
-		// long deserialized = UUIDConverter.deserialize(serialized);
-		// System.out.println(i + " : " + serialized + " : " +
-		// deserialized+" : "+serialized.length());
-		// }
-
 		long runs = (long) 1E6;
 
 		Random rand = new Random();
+
+		/*
+		 * CPU warmup
+		 */
 		long start = System.nanoTime();
 		for (long l = runs; l != 0; l--) {
-			String s = Long.toString(rand.nextLong(), 36);
+			rand.nextLong();
 		}
-		long time = System.nanoTime() - start;
-		System.out.println("Serialization: " + time / runs + " ns");
 
-		// start = System.nanoTime();
-		// for (long l = runs; l != 0; l--) {
-		// String s = new BigInteger(130, random).toString(32);
-		// }
-		// long time = System.nanoTime() - start;
-		// System.out.println("Serialization: " + time / runs + " ns");
+		start = System.nanoTime();
+		for (long l = runs; l != 0; l--) {
+			rand.nextLong();
+		}
+		long randLongTime = (System.nanoTime() - start) / runs;
+
+		start = System.nanoTime();
+		for (long l = runs; l != 0; l--) {
+			rand.nextInt(1 << 9);
+			rand.nextInt(1 << 5);
+			rand.nextInt();
+			rand.nextInt();
+		}
+		long rand4IntTime = (System.nanoTime() - start) / runs;
+
+		start = System.nanoTime();
+		for (long l = runs; l != 0; l--) {
+			String s = MUIDConverter.serialize(rand.nextLong());
+		}
+		long time = (System.nanoTime() - start) / runs - randLongTime;
+		System.out.println("Serialization: " + time + " ns");
+
+		start = System.nanoTime();
+		for (long l = runs; l != 0; l--) {
+			MUIDConverter.getMUID((short) rand.nextInt(1 << 9),
+					(byte) rand.nextInt(1 << 5), rand.nextInt(),
+					(short) rand.nextInt());
+		}
+		long getMUIDTime = (System.nanoTime() - start) / runs;
+		System.out.println("Generation: " + (getMUIDTime - rand4IntTime)
+				+ " ns");
+
+		start = System.nanoTime();
+		for (long l = runs; l != 0; l--) {
+			long uuid = MUIDConverter.getMUID((short) rand.nextInt(1 << 9),
+					(byte) rand.nextInt(1 << 5), rand.nextInt(),
+					(short) rand.nextInt());
+			MUIDConverter.getType(uuid);
+		}
+
+		time = (System.nanoTime() - start) / runs;
+		System.out.println("getType: " + (time - getMUIDTime) + " ns");
 	}
 
-	/**
-	 * Test method for {@link de.metalcon.utils.MUIDConverter#serialize(long)}.
-	 */
 	@Test
 	public void smokeTest() {
-		long runs = (long) 1E2;
 		Random rand = new Random();
-		for (long l = runs; l != 0; l--) {
-			long value = rand.nextLong();
-			String s = MUIDConverter.serialize(value);
+		for (int i = 0; i != 10000; i++) {
+			short oType = (short) (rand.nextInt() & 511);
+			byte oSource = (byte) (rand.nextInt() & 31);
+			int oTimestamp = rand.nextInt();
+			short oID = (short) rand.nextInt();
+
+			long uuid = MUIDConverter.getMUID(oType, oSource, oTimestamp, oID);
+
+			short type = MUIDConverter.getType(uuid);
+			byte source = MUIDConverter.getSource(uuid);
+			int timestamp = MUIDConverter.getTimestamp(uuid);
+			short id = MUIDConverter.getID(uuid);
+
+			if (type != oType) {
+				fail("MUIDConverter.getType(" + uuid + ") returned " + type
+						+ " instead of " + oType);
+			}
+
+			if (source != oSource) {
+				fail("MUIDConverter.getSource(" + uuid + ") returned " + source
+						+ " instead of " + oSource);
+			}
+
+			if (timestamp != oTimestamp) {
+				fail("MUIDConverter.getTimestamp(" + uuid + ") returned "
+						+ timestamp + " instead of " + oTimestamp);
+			}
+
+			if (id != oID) {
+				fail("MUIDConverter.getID(" + uuid + ") returned " + id
+						+ " instead of " + oID);
+			}
+
+			String s = MUIDConverter.serialize(uuid);
 			if (s.length() != MUIDConverter.getMUIDLength()) {
-				fail("UUID with wrong String length generated: " + value + "->"
+				fail("UUID with wrong String length generated: " + uuid + "->"
 						+ s);
 			}
+
 			long deserialized = MUIDConverter.deserialize(s);
-			if (value != deserialized) {
-				fail(value + " has been converted to " + s
+			if (uuid != deserialized) {
+				fail(uuid + " has been converted to " + s
 						+ " but s is deserialized to " + deserialized);
 			}
 		}
