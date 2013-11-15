dd2e9b5a068536f3d6a798cde63bffbdcfcd0d54
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2013-11-15 15:53:57 +0100

    implementation started

--- imageApplicationServer/src/main/java/de/metalcon/imageApplicationServer/ImageApplicationServer.java
+++ imageApplicationServer/src/main/java/de/metalcon/imageApplicationServer/ImageApplicationServer.java
@@ -1,11 +1,45 @@
 package de.metalcon.imageApplicationServer;
 
-import de.metalcon.imageStorageServer.ImageStorageServer;
+import java.io.InputStream;
 
-public class ImageApplicationServer extends ImageStorageServer {
+import org.json.simple.JSONObject;
+
+import de.metalcon.imageStorageServer.ImageStorageServer;
+import de.metalcon.imageStorageServer.ScalingType;
+import de.metalcon.imageStorageServer.protocol.read.ReadResponse;
+
+public class ImageApplicationServer extends ImageStorageServer implements
+		ImageApplicationServerAPI {
 
 	public ImageApplicationServer(final String configFile) {
 		super(configFile);
+	}
+
+	@Override
+	public String readScaledMetaData(final String[] imageIdentifiers,
+			final int width, final int height, final ScalingType scalingType,
+			final ReadResponse response) {
+		final JSONObject scaledMetaData = new JSONObject();
+
+		for (String imageIdentifier : imageIdentifiers) {
+			// TODO: check if image exisiting
+			if (false) {
+				// TODO: load and add meta data
+			} else {
+				// TODO: add error message
+				return null;
+			}
+		}
+
+		return scaledMetaData.toJSONString();
+	}
+
+	@Override
+	public InputStream readThumbnails(final String[] imageIdentifiers,
+			final int width, final int height, final ScalingType scalingType,
+			final ReadResponse response) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
